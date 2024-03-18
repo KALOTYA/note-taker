4c3099d24efbd56baa254a47e4dd6eb20fc23a90
Ajay Kaloty <kalotya99@gmail.com>
2024-03-18 15:54:53 -0700

    Added a slash into the post method

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 
-app.post('api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
   console.info('POST request recieved to add a new note');
 
   const { title, text } = req.body;
